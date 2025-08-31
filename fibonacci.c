8f58c0af90db1a5cffed4c33b56573eed4dd7fce
Prabhav Narayan Pandey <prabhavnarayanpandey@gmail.com>
2025-08-31 16:20:55 +0530

    added a programm

---
+++ fibonacci.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+int fibonacci(int n) {
+    if(n<=2) return 1;
+    return fibonacci(n-1) + fibonacci(n-2);
+}
+
+int main() {
+    
+   int n;
+   printf("Enter a number : ");
+   scanf("%d",&n);
+
+   int x = fibonacci(n);
+
+   printf("The %dth fibonacci is : %d",n,x);
+ 
+    return 0;
+}
