51e34ad7e193a169d4fc63655160b8d739b56d9b
Prabhav Narayan Pandey <prabhavnarayanpandey@gmail.com>
2025-09-03 20:28:47 +0530

    added new folder and programm

---
+++ Arrays.c/ArrayBasics.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+int main() {
+ 
+// TO PRINT ARRAYS 
+
+char arr [5] = {'@','%','^','&','*'};
+ for(int i=0;i<=4;i++) printf(" %c ",arr[i]);
+
+ 
+    // TAKING INPUT FROM USER
+   /*
+    int n;
+    printf("Enter size of array: ");
+    scanf("%d",&n);
+
+    int arr[n];
+
+    for (int i = 0; i < n; i++) {
+        printf("Enter element %d: ", i + 1);
+        scanf("%d", &arr[i]);
+    }
+    
+    printf("The array elements are: ");
+    for (int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+    
+    return 0;
+}
+*/
+   return 0;
+} 
